15a51a2c198918996df811c85c85029f1e10ba68
Sekkos03 <Sekoukosiah82@gmail.com>
2025-12-29 09:39:06 +0100

    1.0.4

--- apps/public-web/src/pages/Events.tsx
+++ apps/public-web/src/pages/Events.tsx
@@ -50,7 +50,7 @@ export default function Events() {
         <section className="photosHero" aria-label="Intro">
           <div className="photosHero__banner">
             <img
-              src="public/Liberian%20Headlines.png"
+              src="src/assets/Liberian Headlines.png"
               alt="Liberian headlines"
               className="heroBanner__img"
             />
