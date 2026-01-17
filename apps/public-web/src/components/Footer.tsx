9fe0affe3a7248010d721d4b244c6ff5e00f5ed0
Sekkos03 <Sekoukosiah82@gmail.com>
2026-01-17 04:04:48 +0100

    2.0.3

--- apps/public-web/src/components/Footer.tsx
+++ apps/public-web/src/components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
           Copyright © {year} Liberia organization Oslo Norway · Webdesign by{" "}
           <a
             className="site-footer__link"
-            href="https://www.linkedin.com/in/sekou-kosiah/"
+            href="https://www.linkedin.com/in/sekou-kosiah-93b96325a/"
             rel="noopener noreferrer"
             target="_blank"
           >
